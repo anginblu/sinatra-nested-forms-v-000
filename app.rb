4eefb6afba003da79d3411f54f2a2c3ac2238245
Jacqueline Lee <anginblu@gmail.com>
2018-01-06 05:44:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,10 +2,18 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-
     # code other routes/actions here
     get '/' do
 
     end
+
+    get '/new' do
+
+    end
+
+    post '/pirates' do
+      
+    end
+
   end
 end
