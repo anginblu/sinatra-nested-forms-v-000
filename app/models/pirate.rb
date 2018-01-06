0cc823364391c6d69d0be8ff422fe61ec63d5729
Jacqueline Lee <anginblu@gmail.com>
2018-01-06 05:41:54 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,16 +1,16 @@
 class Pirate
-  attr_reader :name, :grade
- 
+  attr_reader :name, :weight, :height
+
  STUDENTS = []
- 
+
   def initialize(params)
     @name = params[:name]
     @grade = params[:grade]
     STUDENTS << self
   end
- 
+
   def self.all
     STUDENTS
   end
- 
+
 end
